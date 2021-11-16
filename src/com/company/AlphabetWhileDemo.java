b12427197c323c4644ee3afb1b44124f2d9b08f8
Skynet_u <Vanes_kote@mail.ru>
2021-11-16 11:38:35 +0300

    AlphabetWhileDemo

---
+++ src/com/company/AlphabetWhileDemo.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class AlphabetWhileDemo {
+    public static void main(String[] args) {
+        char ch = 'a';
+        while (ch <= 'я') {
+            System.out.println(ch);
+            ch++;
+        }
+    }
+}
